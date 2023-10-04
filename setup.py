260ece87e34b9986d72a1ee7ac8393ae8cb85d6d
Kobi Felton <kobi.c.f@gmail.com>
2023-10-04 08:15:26 -0400

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='moleculeresolver',
-    version='0.1',
-    author='Simon Müller',
-    author_email='simon.mueller@tuhh.de',
-    packages=['moleculeresolver'],
-    url='https://collaborating.tuhh.de/v-8/moleculeresolver.git',
-    license='LICENSE.txt',
-    description='A package to use several web services to find molecule structures, synonyms and CAS.',
-    long_description=open('README.md').read(),
-    install_requires=['prompt_toolkit', 'regex', 'rdkit', 'tqdm', 'urllib3', 'xmltodict', 'xyz2mol']
+    name="moleculeresolver",
+    version="0.1",
+    author="Simon Müller",
+    author_email="simon.mueller@tuhh.de",
+    packages=["moleculeresolver"],
+    url="https://github.com/MoleculeResolver/molecule-resolver",
+    license="LICENSE.txt",
+    description="A package to use several web services to find molecule structures, synonyms and CAS.",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "prompt_toolkit",
+        "regex",
+        "rdkit",
+        "requests",
+        "openpyxl",
+        "tqdm",
+        "urllib3",
+        "xmltodict",
+    ],
 )
