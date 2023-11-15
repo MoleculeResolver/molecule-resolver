18de1a985cc002d77845d778cbd9b8ecc5706062
Kobi Felton <kobi.c.f@gmail.com>
2023-11-15 07:54:44 -0500

    IUPAC integration tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,17 +2,23 @@ import pytest
 from moleculeresolver import MoleculeResolver
 import json
 
-with open("benchmark_component_molecules.json", "r") as f:
+# IUPAC names
+with open("benchmark_component_molecules_iupac.json", "r") as f:
     benchmark = json.load(f)
-    breakpoint()
 
-@pytest.mark.parametrize("name, data", benchmark.items())
-def test_opsin(name, data):
+
+@pytest.mark.parametrize("data", benchmark.values())
+def test_opsin(data):
     with MoleculeResolver() as mr:
-        res = mr.get_molecule_from_OPSIN(name)
+        iupac_name = data["iupac_name"]
+        res = mr.get_molecule_from_OPSIN(iupac_name)
         if res is None:
-            assert data["SMILES"] == "None"
-        assert res.SMILES == data["SMILES"]
+            raise ValueError("No molecule found for " + iupac_name)
+        if res.SMILES == data["SMILES"]:
+            return
+        else:
+            raise ValueError("Expected " + data["SMILES"] + " but got " + res.SMILES)
+
 
 if __name__ == "__main__":
     test_opsin()
