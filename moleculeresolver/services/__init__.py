87c9edd22793d5aef9ceced770fe61b2ea4db51e
Tomodovodoo <tomthegreatest04@gmail.com>
2026-02-28 18:11:01 +0100

    feat(services): add ServiceAdapter ABC and registry

---
+++ moleculeresolver/services/__init__.py
@@ -0,0 +1,8 @@
+from .base import ServiceAdapter, ServiceSearchResult
+from .registry import ServiceAdapterRegistry
+
+__all__ = [
+    "ServiceAdapter",
+    "ServiceSearchResult",
+    "ServiceAdapterRegistry",
+]
