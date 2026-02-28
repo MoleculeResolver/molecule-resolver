ceba560e90882bccc432b6c71cb50281a825dec5
Tomodovodoo <tomthegreatest04@gmail.com>
2026-02-28 18:18:53 +0100

    feat(scoring): add candidate grouping and score model

---
+++ moleculeresolver/resolution/__init__.py
@@ -0,0 +1,11 @@
+from .models import (
+    StructureGroupCandidate,
+    WeightedStructureScore,
+    build_structure_group_candidates,
+)
+
+__all__ = [
+    "StructureGroupCandidate",
+    "WeightedStructureScore",
+    "build_structure_group_candidates",
+]
